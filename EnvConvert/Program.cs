0a2b453daf650b1a18b7a14f036e892b8d2d6a5c
phwt <28344318+phwt@users.noreply.github.com>
2022-08-27 22:23:07 +0700

    feat: handle input/output

--- EnvConvert/Program.cs
+++ EnvConvert/Program.cs
@@ -4,22 +4,38 @@ namespace EnvConvert
 {
     internal class Program
     {
+        private static void InputOutputHandler(FileInfo file, string? outputOption, Func<string, string> converter)
+        {
+            var result = converter(File.ReadAllText(file.FullName));
+            if (String.IsNullOrWhiteSpace(outputOption))
+            {
+                System.Console.WriteLine(result);
+            }
+            else
+            {
+                File.WriteAllText(outputOption, result);
+            }
+        }
+
         private static async Task<int> Main(string[] args)
         {
-            var fileOption = new Option<FileInfo?>(new string[] { "--file", "-f" }, "Input file");
-
+            // Root and subcommands
             var rootCommand = new RootCommand("CLI Tools for converting configurations into environment variables format");
-            rootCommand.AddOption(fileOption);
-            rootCommand.SetHandler((file) =>
-            {
-                if (file != null)
-                {
-                    var jsonString = File.ReadAllText(file.FullName);
-                    var result = new Converter().FromJSON(jsonString);
-                    System.Console.WriteLine(result);
-                }
-            },
-            fileOption);
+            var jsonCommand = new Command("json", "Convert from JSON format");
+
+            rootCommand.Add(jsonCommand);
+
+            // Global options
+            var fileOption = new Option<FileInfo>(new string[] { "--file", "-f" }, "Path to the input file");
+            var outputOption = new Option<string?>(new string[] { "--out", "-o" }, "Output file - defaults to STDOUT if unspecified");
+
+            rootCommand.AddGlobalOption(fileOption);
+            rootCommand.AddGlobalOption(outputOption);
+
+            // Handlers
+            jsonCommand.SetHandler((fileOption, outputOption) =>
+                InputOutputHandler(fileOption, outputOption, new Converter().FromJSON),
+            fileOption, outputOption);
 
             return await rootCommand.InvokeAsync(args);
         }
